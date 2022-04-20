a73286b9499303198766cda0167fbb51b2385fa8
dleins <dleins@techfak.uni-bielefeld.de>
2022-04-20 11:33:21 +0200

    Adds jacobian tests

---
+++ test/main.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+#include <ros/ros.h>
+
+int main(int argc, char **argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+	ros::init(argc, argv, "franka_hw_mujoco_test_node");
+	return RUN_ALL_TESTS();
+}
